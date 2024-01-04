6d945827408a49b9470127fb52d6f5866b4022eb
TheDim0n <>
2024-01-04 22:48:35 +0300

    resolve TODO; some code refactoring; update tests; add tests workflow

--- lib/src/game.cpp
+++ lib/src/game.cpp
@@ -5,10 +5,16 @@ Game::Game::Game(std::string solution, uint8_t attemptions) {
     this->solution = solution;
     this->attemptions = attemptions;
     this->word_size = solution.size();
-    this->states = {0, 0, 0, 0, 0};
+    this->states = {
+        constants::State::undefined,
+        constants::State::undefined,
+        constants::State::undefined,
+        constants::State::undefined,
+        constants::State::undefined
+    };
 }
 
-const bool Game::Game::check_attemption(const std::string& attemption_word) {
+const bool Game::Game::checkAttemption(const std::string& attemption_word) {
     bool status = true;
     for (size_t i = 1; i < solution.size(); i += 2) {
         const size_t stateIndex = i / 2;
@@ -16,22 +22,22 @@ const bool Game::Game::check_attemption(const std::string& attemption_word) {
             this->solution[i-1] == attemption_word[i-1]
             && this->solution[i] == attemption_word[i]
         );
-        if (isEquial) this->states[stateIndex] = 1;
+        if (isEquial) this->states[stateIndex] = constants::State::inCorrectPos;
         else {
             status = false;
             size_t index = this->solution.find(attemption_word[i]);
-            if (index == std::string::npos) this->states[stateIndex] = 3;
-            else this->states[stateIndex] = 2;
+            if (index == std::string::npos) this->states[stateIndex] = constants::State::notFound;
+            else this->states[stateIndex] = constants::State::inAnoterPos;
         }
     }
     this->attemptions--;
     return status;
 }
 
-const std::vector<uint8_t>& Game::Game::get_states() {
+const std::array<constants::State, 5>& Game::Game::getStates() {
     return this->states;
 };
 
-const uint8_t& Game::Game::get_attemption() {
+const uint8_t& Game::Game::getAttemption() {
     return this->attemptions;
 }
