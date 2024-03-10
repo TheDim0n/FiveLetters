53bec1491a28dfe364b4c5ee3754fe2bd13dc017
TheDim0n <>
2024-03-10 21:24:23 +0300

    update window

--- src/bin/desktop/main.rs
+++ src/bin/desktop/main.rs
@@ -1,14 +1,24 @@
 use winit::{
+    dpi::LogicalSize,
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
+    keyboard::NamedKey,
+    window::WindowBuilder
 };
 
 
 fn main() {
     let event_loop = EventLoop::new().unwrap();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
-    window.set_title("5 букв");
+    let window_builder = WindowBuilder::new()
+        .with_title("5 букв")
+        .with_resizable(false)
+        .with_maximized(false)
+        .with_inner_size(LogicalSize::<i32>::from((300, 1200)))
+        .with_min_inner_size(LogicalSize::<i32>::from((300, 1200)))
+        .with_max_inner_size(LogicalSize::<i32>::from((300, 1200)))
+        .with_transparent(true)
+        .with_decorations(false);
+    let window = window_builder.build(&event_loop).unwrap();
     event_loop.set_control_flow(ControlFlow::Wait);
     event_loop.run(move |event, elwt| {
         match event {
@@ -38,6 +48,15 @@ fn main() {
                 // It's preferable for applications that do not render continuously to render in
                 // this event rather than in AboutToWait, since rendering in here allows
                 // the program to gracefully handle redraws requested by the OS.
+            },
+            Event::WindowEvent {
+                event: WindowEvent::KeyboardInput { event, ..},
+                ..
+            } => {
+                match event.logical_key {
+                    winit::keyboard::Key::Named(NamedKey::Escape) => elwt.exit(),
+                    _ => ()
+                }
             },
             _ => ()
         }
